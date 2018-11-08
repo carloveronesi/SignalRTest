6a468eb3b0dcc370bbd68d8cb06621f06a82d91b
c.veronesi <veronesicarlo@outlook.it>
2018-11-08 14:45:25 +0100

    Init di AppController

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  OnInit
+} from '@angular/core';
+import {
+  HubConnection, HubConnectionBuilder
+} from '@aspnet/signalr'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  ngOnInit() {
+    //HubConnection ha un costruttore privato. Per crearlo devo usare ConnectionBuilder
+    let builder = new HubConnectionBuilder();
+    builder.withUrl('http://localhost:5000/api/message');
+    //Poi buildo connection
+    var connection = builder.build();
+
+    connection.on('send', data => {
+      console.log(data);
+    });
+
+    connection.start()
+      .then(() => connection.invoke('send', 'Hello'));
+  }
 }
