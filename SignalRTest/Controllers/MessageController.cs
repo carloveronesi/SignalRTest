7a14b67c70e458e3ac4f558d922bf6f93649c54f
c.veronesi <veronesicarlo@outlook.it>
2018-11-08 11:07:50 +0100

    Aggiunta risposta OK

---
+++ SignalRTest/Controllers/MessageController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SignalR1.Controllers
+{
+	[Route("api/message")]
+	public class MessageController : Controller
+	{
+		public IActionResult Post()
+		{
+			// Broadcast the message to clients 
+			return Ok();
+		}
+	}
+}
---
+++ SignalRTest/Hubs/MessageHub.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRTest
+{
+	public class MessageHub : Hub
+	{
+		public Task Send(string message)
+		{
+			return Clients.All.SendAsync("Send", message);
+		}
+	}
+}
